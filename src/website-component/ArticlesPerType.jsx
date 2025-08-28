89409f5a2e3a771b4b8442cc9c5602b48a8e79dd
Muhammad Alyousef <muhammadabdalrahman2003@gmail.com>
2025-08-28 14:54:29 +0300

    New Update

--- src/website-component/ArticlesPerType.jsx
+++ src/website-component/ArticlesPerType.jsx
@@ -1,42 +1,40 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 const ArticlesPerType = ({ article }) => {
-  const { authName, arTitle, arBody, arImg } = article;
+  const { authName, arTitle, arBody, arImg, aID } = article;
   return (
     <>
       {/* <div className={`border-b-gray-100`}> */}
-        <div className="flex items-center gap-x-3">
-          <span className="flex flex-col gap-y-1.5 pe-10">
-            <a
-              href="#"
-              className="flex items-center w-fit gap-x-3 text-gray-300 hover:text-gray-800"
-            >
-              {/* <span className="size-6 rounded-full overflow-hidden">
-                <img src={authImg} alt="" />
-              </span> */}
-              <span className="text-sm">{authName}</span>
-            </a>
-            <a
-              href="#"
-              className="text-xl font-semibold line-clamp-1! relative w-fit"
-            >
-              {arTitle}
-            </a>
-            <a href="#" className="text-sm text-gray-700 line-clamp-1!">
-              {arBody}
-            </a>
-          </span>
+      <div className="flex items-center gap-x-3">
+        <span className="flex flex-col gap-y-1.5 pe-10">
           <a
             href="#"
-            className="w-40 h-28 shrink-0 order-first overflow-hidden"
+            className="flex items-center w-fit gap-x-3 text-gray-300 hover:text-gray-800"
           >
-            <img
-              className="w-full h-full object-cover hover:scale-110 transition"
-              src={`${arImg}`}
-              alt=""
-            />
+            {/* <span className="size-6 rounded-full overflow-hidden">
+                <img src={authImg} alt="" />
+              </span> */}
+            <span className="text-sm">{authName}</span>
           </a>
-        </div>
+          <NavLink
+            to={`/article-details/${aID}`}
+            className="text-xl font-semibold line-clamp-1! relative w-fit"
+          >
+            {arTitle}
+          </NavLink>
+          <a href="#" className="text-sm text-gray-700 line-clamp-1!">
+            {arBody}
+          </a>
+        </span>
+        <a href="#" className="w-40 h-28 shrink-0 order-first overflow-hidden">
+          <img
+            className="w-full h-full object-cover hover:scale-110 transition"
+            src={`${arImg}`}
+            alt=""
+          />
+        </a>
+      </div>
       {/* </div> */}
     </>
   );
